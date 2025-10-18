cf6c5bc49881a3f30dad421da1468d66d095ed28
Dawid Komęza <dawid.komeza@gmail.com>
2025-10-18 21:07:20 +0200

    🎉 Initial commit

---
+++ init.lua
@@ -0,0 +1,48 @@
+-- dkomeza neovim minimal setup -- 
+----------------------------------
+
+-- leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+
+-- window keymap
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- Plugins
+require("lazy").setup({
+	import = 'plugins'
+})
+
