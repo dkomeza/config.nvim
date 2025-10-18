3bf2691de63af8a7cca82ef3d4033b48e3372971
Dawid Komęza <dawid.komeza@gmail.com>
2025-10-18 21:41:24 +0200

    ✨ Add open file plugin (CMD+P)

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
+----------------------------------
 -- dkomeza neovim minimal setup -- 
 ----------------------------------
 
 -- leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true 
 
 -- line numbers
 vim.o.number = true
@@ -16,6 +18,20 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- clipboard handling
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+-- time handling
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
 
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
